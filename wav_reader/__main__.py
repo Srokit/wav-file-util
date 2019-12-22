2b5c6887c8656f5ee7919c69924aab521260afa9
Stan Rokita <srok35@gmail.com>
2019-12-22 14:38:46 -0800

    Refactor create wav file w trans to static method

--- wav_reader/__main__.py
+++ wav_reader/__main__.py
@@ -13,7 +13,7 @@ def main():
     """
     # argv[1] should be the wave filename
     wav_file = WavFile(argv[1])
-    print("Wav file has no meta data errors!")
+    wav_file.read_meta_data_from_disk()
 
     def remove_channel1(channel_index, sample_value):
         if channel_index == 1:
@@ -27,5 +27,9 @@ def main():
         else:
             return sample_value
 
-    wav_file.create_new_wav_file_with_transformation(remove_channel0, "test/wav_files/out/vocal_loop_1_no_right_channel.wav")
-    wav_file.create_new_wav_file_with_transformation(remove_channel1, "test/wav_files/out/vocal_loop_1_no_left_channel.wav")
+    no_channel0_wav_file = WavFile("test/wav_files/out/vocal_loop_1_no_channel0.wav")
+    no_channel1_wav_file = WavFile("test/wav_files/out/vocal_loop_1_no_channel1.wav")
+    WavFile.create_new_wav_file_with_transformation(wav_file, no_channel0_wav_file, remove_channel0)
+    print("Created wav file: test/wav_files/out/vocal_loop_1_no_channel0.wav")
+    WavFile.create_new_wav_file_with_transformation(wav_file, no_channel1_wav_file, remove_channel1)
+    print("Created wav file: test/wav_files/out/vocal_loop_1_no_channel1.wav")
--- wav_reader/wav_file.py
+++ wav_reader/wav_file.py
@@ -3,6 +3,7 @@
 """
 
 import struct
+import copy
 
 
 class WavFileMetaData:
@@ -44,6 +45,33 @@ class WavFileMetaData:
 
     DATA_CHUNK_ID_EXPECTED = 0x64617461 # "data" in ascii (Big-endian)
 
+    NUM_SAMPLES_DEFAULT = 2000000 # 2 Mill TODO: Make this more appropriate
+    FORMAT_CHUNK_SAMPLE_RATE_DEFAULT = 8000 # 8 kHz TODO: Make better
+    BYTES_PER_SAMPLE_DEFAULT = 3 # Because that was the number I saw in one wav file
+
+    @classmethod
+    def make_default_meta_data(cls):
+        bytes_of_audio_data = cls.NUM_SAMPLES_DEFAULT * cls.FORMAT_CHUNK_NUM_CHANNELS_EXPECTED * cls.BYTES_PER_SAMPLE_DEFAULT
+        meta_data = WavFileMetaData()
+        meta_data.super_chunk_id = cls.SUPER_CHUNK_ID_EXPECTED
+        # There are 8 (super_chunk_id and super_chunk_size itself) not included in super_chunk_size
+        meta_data.super_chunk_size = cls.NUM_BYTES_BEFORE_DATA - 8 + bytes_of_audio_data
+        meta_data.super_chunk_format = cls.SUPER_CHUNK_FORMAT_EXPECTED
+        meta_data.format_chunk_id = cls.FORMAT_CHUNK_ID_EXPECTED
+        meta_data.format_chunk_size = cls.FORAMT_CHUNK_SIZE_EXPECTED
+        meta_data.format_chunk_audio_format = cls.FORMAT_CHUNK_AUDIO_FORMAT_EXPECTED
+        meta_data.format_chunk_num_channels = cls.FORMAT_CHUNK_NUM_CHANNELS_EXPECTED
+        meta_data.format_chunk_sample_rate = cls.FORMAT_CHUNK_SAMPLE_RATE_DEFAULT
+
+        meta_data.format_chunk_byte_rate = cls.FORMAT_CHUNK_SAMPLE_RATE_DEFAULT * \
+                cls.BYTES_PER_SAMPLE_DEFAULT * cls.FORMAT_CHUNK_NUM_CHANNELS_EXPECTED
+        meta_data.format_chunk_block_align = cls.BYTES_PER_SAMPLE_DEFAULT \
+                * cls.FORMAT_CHUNK_NUM_CHANNELS_EXPECTED
+        meta_data.format_chunk_bits_per_sample = cls.BYTES_PER_SAMPLE_DEFAULT * 8
+        meta_data.data_chunk_id = cls.DATA_CHUNK_ID_EXPECTED
+        meta_data.data_chunk_size = bytes_of_audio_data 
+        return meta_data
+
     def __init__(self):
         self.super_chunk_id = 0x0
         self.super_chunk_size = 0x0
@@ -66,44 +94,53 @@ class WavFile:
 
     """
 
+    # How many audio samples are loaded into memory at a time when streaming wav file data
     SAMPLES_PER_BLOCK = 1000
+
+    @classmethod
+    def create_new_wav_file_with_transformation(cls, src_wav_file, dest_wav_file, trans_func=None):
+        """Apply trans_func to each sample and all the channels for that sample if
+        trans_func is None then the new wav file will be an exact copy.
+        trans_func should accept channel_index: int and sample_value: int as paramaters
+        uses src_wav_file as file data input and dest_wav_file as the file to output to
+        """
+        write_file = open(dest_wav_file.filename, 'wb')
+        # Copy the meta data object from source file
+        dest_wav_file.meta_data = copy.deepcopy(src_wav_file.meta_data)
+        dest_wav_file.meta_data_bytes = src_wav_file.meta_data_bytes
+        # Write the meta data to disk
+        write_file.write(dest_wav_file.meta_data_bytes)
+        with open(src_wav_file.filename, 'rb') as wav_file_obj:
+            wav_file_obj.seek(WavFileMetaData.NUM_BYTES_BEFORE_DATA_STARTS)
+            bytes_per_block = src_wav_file._get_read_block_size()
+            struct_format_str, padding = src_wav_file._get_per_sample_struct_format_str_and_padding()
+            while True:
+                data = bytearray(wav_file_obj.read(bytes_per_block))
+                channel_index = 0
+                bytes_per_sample = src_wav_file.meta_data.format_chunk_bits_per_sample // 8
+                for sample_offset in range(0, len(data), bytes_per_sample):
+                    sample_value = int(struct.unpack_from(struct_format_str, data[sample_offset:sample_offset+bytes_per_sample]+padding)[0])
+                    channel_index = (channel_index + 1) % src_wav_file.meta_data.format_chunk_num_channels
+                    if trans_func is not None:
+                        sample_value = trans_func(channel_index, sample_value)
+                    data[sample_offset:sample_offset+bytes_per_sample] = struct.pack(struct_format_str, sample_value)[:-len(padding)]
+                    write_file.write(bytes(data[sample_offset:sample_offset+bytes_per_sample])) 
+                if len(data) < bytes_per_block:
+                    break
+        write_file.close()
 
     def __init__(self, filename):
         self.filename = filename
+        self.meta_data = None
+
+
+    def read_meta_data_from_disk(self):
         with open(self.filename, 'rb') as wav_file_obj:
             # Meta data bytes is used in applying transformations as the meta data bytes are copied
             self.meta_data_bytes = wav_file_obj.read(WavFileMetaData.NUM_BYTES_BEFORE_DATA_STARTS)
         self.meta_data = self._parse_meta_data(self.meta_data_bytes)
         is_meta_data_valid, err_str = self._validate_meta_data()
         assert is_meta_data_valid, err_str
-
-    def create_new_wav_file_with_transformation(self, trans_func, out_filename):
-        """Apply trans_func to each sample and all the channels for that sample.
-        trans_func should accept channel_index: int and sample_value: int as paramaters
-        returns a WavFile object for the newly created file.
-        """
-        write_file = open(out_filename, 'wb')
-        # Write an exact copy of the meta data in current wav_file as another wav file
-        write_file.write(self.meta_data_bytes)
-        with open(self.filename, 'rb') as wav_file_obj:
-            wav_file_obj.seek(WavFileMetaData.NUM_BYTES_BEFORE_DATA_STARTS)
-            bytes_per_block = self._get_read_block_size()
-            struct_format_str, padding = self._get_per_sample_struct_format_str_and_padding()
-            while True:
-                data = bytearray(wav_file_obj.read(bytes_per_block))
-                channel_index = 0
-                bytes_per_sample = self.meta_data.format_chunk_bits_per_sample // 8
-                for sample_offset in range(0, len(data), bytes_per_sample):
-                    sample_value = int(struct.unpack_from(struct_format_str, data[sample_offset:sample_offset+bytes_per_sample]+padding)[0])
-                    channel_index = (channel_index + 1) % self.meta_data.format_chunk_num_channels
-                    sample_value = trans_func(channel_index, sample_value)
-                    data[sample_offset:sample_offset+bytes_per_sample] = struct.pack(struct_format_str, sample_value)[:-len(padding)]
-                    write_file.write(bytes(data[sample_offset:sample_offset+bytes_per_sample])) 
-                if len(data) < bytes_per_block:
-                    break
-        write_file.close()
-        new_wav_file = WavFile(out_filename)
-        return new_wav_file
 
     # Private helpers
 
