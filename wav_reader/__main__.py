b6a4842b46360a693f4c23341b403c59ec5de14f
Stan Rokita <srok35@gmail.com>
2019-12-22 12:25:20 -0800

    Add method to create dup WavFile with transformation
    Add a method to WavFile to create duplicate wav file and write to it
    on disc given a transformation function that takes in the channel index
    and sample value as parameters and returns a new sample value.

--- wav_reader/__main__.py
+++ wav_reader/__main__.py
@@ -13,11 +13,19 @@ def main():
     """
     # argv[1] should be the wave filename
     wav_file = WavFile(argv[1])
-    is_meta_data_valid, meta_data_errors = wav_file.validate_meta_data()
-    if not is_meta_data_valid:
-        print("Wav file meta data errors:")
-        for err in meta_data_errors:
-            print(err)
-    else:
-        print("Wav file has no meta data errors!")
+    print("Wav file has no meta data errors!")
 
+    def remove_channel1(channel_index, sample_value):
+        if channel_index == 1:
+            return 0
+        else:
+            return sample_value
+
+    def remove_channel0(channel_index, sample_value):
+        if channel_index == 0:
+            return 0
+        else:
+            return sample_value
+
+    wav_file.create_new_wav_file_with_transformation(remove_channel0, "test/wav_files/out/vocal_loop_1_no_right_channel.wav")
+    wav_file.create_new_wav_file_with_transformation(remove_channel1, "test/wav_files/out/vocal_loop_1_no_left_channel.wav")
--- wav_reader/wav_file.py
+++ wav_reader/wav_file.py
@@ -66,15 +66,49 @@ class WavFile:
 
     """
 
+    SAMPLES_PER_BLOCK = 1000
+
     def __init__(self, filename):
+        self.filename = filename
+        with open(self.filename, 'rb') as wav_file_obj:
+            # Meta data bytes is used in applying transformations as the meta data bytes are copied
+            self.meta_data_bytes = wav_file_obj.read(WavFileMetaData.NUM_BYTES_BEFORE_DATA_STARTS)
+        self.meta_data = self._parse_meta_data(self.meta_data_bytes)
+        is_meta_data_valid, err_str = self._validate_meta_data()
+        assert is_meta_data_valid, err_str
 
-        with open(filename, 'rb') as wav_file_obj:
-            meta_data_bytes = wav_file_obj.read(WavFileMetaData.NUM_BYTES_BEFORE_DATA_STARTS)
-        self.meta_data = self._parse_meta_data(meta_data_bytes)
+    def create_new_wav_file_with_transformation(self, trans_func, out_filename):
+        """Apply trans_func to each sample and all the channels for that sample.
+        trans_func should accept channel_index: int and sample_value: int as paramaters
+        returns a WavFile object for the newly created file.
+        """
+        write_file = open(out_filename, 'wb')
+        # Write an exact copy of the meta data in current wav_file as another wav file
+        write_file.write(self.meta_data_bytes)
+        with open(self.filename, 'rb') as wav_file_obj:
+            wav_file_obj.seek(WavFileMetaData.NUM_BYTES_BEFORE_DATA_STARTS)
+            bytes_per_block = self._get_read_block_size()
+            struct_format_str, padding = self._get_per_sample_struct_format_str_and_padding()
+            while True:
+                data = bytearray(wav_file_obj.read(bytes_per_block))
+                channel_index = 0
+                bytes_per_sample = self.meta_data.format_chunk_bits_per_sample // 8
+                for sample_offset in range(0, len(data), bytes_per_sample):
+                    sample_value = int(struct.unpack_from(struct_format_str, data[sample_offset:sample_offset+bytes_per_sample]+padding)[0])
+                    channel_index = (channel_index + 1) % self.meta_data.format_chunk_num_channels
+                    sample_value = trans_func(channel_index, sample_value)
+                    data[sample_offset:sample_offset+bytes_per_sample] = struct.pack(struct_format_str, sample_value)[:-len(padding)]
+                    write_file.write(bytes(data[sample_offset:sample_offset+bytes_per_sample])) 
+                if len(data) < bytes_per_block:
+                    break
+        write_file.close()
+        new_wav_file = WavFile(out_filename)
+        return new_wav_file
 
+    # Private helpers
 
-    def validate_meta_data(self):
-        """Returns bool, List[string] combo. The list of strings are the error
+    def _validate_meta_data(self):
+        """Returns bool, str combo. The string is the error(s)
             that occured or None if no errors. The bool is true on no errors but
             false if there were errors.
         """
@@ -108,10 +142,11 @@ class WavFile:
             success = False
             errors.append("data_chunk_id | actual = %d expected = %d" %
                     (self.meta_data.data_chunk_id, WavFileMetaData.DATA_CHUNK_ID_EXPECTED))
-        return success, errors or None
-
-
-    # Priavte helpers
+        if self.meta_data.format_chunk_bits_per_sample % 8 != 0:
+            success = False
+            errors.append("format_chunk_bits_per_sample | actual: Not divisble by 8 expected: divisible by 8")
+        err_str = "Wav file metadata errors:\n  " + '\n  '.join(errors) if errors else None
+        return success, err_str
 
     def _parse_meta_data(self, meta_data_bytes):
         """Helpers used in ctor that creates the WavFileMetaData object used by WavFile class.
@@ -138,7 +173,7 @@ class WavFile:
                 meta_data_bytes[WavFileMetaData.FORMAT_CHUNK_BYTE_RATE_OFFSET:WavFileMetaData.FORMAT_CHUNK_BYTE_RATE_OFFSET + 4])[0]
         meta_data.format_chunk_block_align = struct.unpack_from('<H',
                 meta_data_bytes[WavFileMetaData.FORMAT_CHUNK_BLOCK_ALIGN_OFFSET:WavFileMetaData.FORMAT_CHUNK_BLOCK_ALIGN_OFFSET + 2])[0]
-        meta_data.format_chunk_bits_per_sample_offset = struct.unpack_from('<H',
+        meta_data.format_chunk_bits_per_sample = struct.unpack_from('<H',
                 meta_data_bytes[WavFileMetaData.FORMAT_CHUNK_BITS_PER_SAMPLE_OFFSET:WavFileMetaData.FORMAT_CHUNK_BITS_PER_SAMPLE_OFFSET + 2])[0]
         meta_data.data_chunk_id = struct.unpack_from('>I',
                 meta_data_bytes[WavFileMetaData.DATA_CHUNK_ID_OFFSET:WavFileMetaData.DATA_CHUNK_ID_OFFSET + 4])[0]
@@ -146,3 +181,31 @@ class WavFile:
                 meta_data_bytes[WavFileMetaData.DATA_CHUNK_SIZE_OFFSET:WavFileMetaData.DATA_CHUNK_SIZE_OFFSET + 4])[0]
         return meta_data
 
+    def _get_read_block_size(self):
+        """Returns the number of bytes that should be processed at a time from
+        the wav file which is based on some of the metadata so that we do not
+        unalign with the samples.
+        """
+        return WavFile.SAMPLES_PER_BLOCK * self.meta_data.format_chunk_num_channels * self.meta_data.format_chunk_bits_per_sample // 8
+
+    def _get_per_sample_struct_format_str_and_padding(self):
+        """Returns the string that should be used as the first argument to
+        struct.unpack_from because this will differ depending on the number of
+        bits per sample as well as the padding of 0's bytes that is required for
+        the pack to work.
+
+        """
+        bytes_per_sample = self.meta_data.format_chunk_bits_per_sample // 8
+        if bytes_per_sample == 1:
+            fmt = '<c'
+            padding = bytearray(0)
+        if bytes_per_sample == 2:
+            fmt = '<H'
+            padding = bytearray(0)
+        if bytes_per_sample <= 4:
+            fmt = '<I'
+            padding = bytearray(4 - bytes_per_sample)
+        else: # bytes_per_sample > 4
+            fmt = bytearray(8 - bytes_per_sample)
+            padding = bytearray(8 - bytes_per_sample)
+        return fmt, padding
